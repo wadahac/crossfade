847cf12a141c76f863708a3cfc867808e80bf37b
wadahac <divyanshbhandari328@gmail.com>
2026-05-01 14:52:41 +0530

    added "god mode " type thing for cycle later on
    press shift + ctrl + keypad asterisk to enter the mode

--- scenes/player/code/player.gd
+++ scenes/player/code/player.gd
@@ -17,14 +17,19 @@ const ANIM_WALK_NORTH = "walk_north"
 enum PlayerState {
 	IDLE = 0,
 	WALKING = 1,
+	CYCLEIDLE = 2,
+	CYCLERUNNING = 3
 }
 
+var have_cycle := false
 
 var direction: Vector2
 var last_direction: Vector2 = Vector2.DOWN # Store last non-zero direction for idle animation
 var current_player_state: PlayerState
 var speed : int = 100
+
 const SPEED_MULTIPLIER = 100  # Adjust this to control how fast the character moves
+const CYCLESPEEDMULTIPLIER = 2.5 #Adjust this to control the speed of cycle
 
 func get_cardinal_direction(input_vector: Vector2) -> Vector2:
 	if input_vector == Vector2.ZERO:
@@ -55,11 +60,19 @@ func get_cardinal_input() -> Vector2:
 	
 	return input
 
-
+func returnspeedmultiplier(inputmultiplier):
+	if have_cycle:
+		return inputmultiplier * CYCLESPEEDMULTIPLIER
+	else:
+		return inputmultiplier
 
 func _ready() -> void:
 	current_player_state = PlayerState.IDLE
 	animated_sprite.play(ANIM_IDLE_SOUTH)
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("DEBUG"):
+		have_cycle = !have_cycle
 
 func _physics_process(delta: float) -> void:
 	# Get cardinal input (no diagonal movement)🥀
@@ -71,11 +84,14 @@ func _physics_process(delta: float) -> void:
 		last_direction = direction
 	
 	# Update movement
-	velocity = direction * speed * delta * SPEED_MULTIPLIER
+	velocity = direction * speed * delta * returnspeedmultiplier(SPEED_MULTIPLIER)
 	move_and_slide()
 	
 	# Update player state based on movement
-	current_player_state = PlayerState.WALKING if direction != Vector2.ZERO else PlayerState.IDLE
+	if have_cycle :
+		current_player_state = PlayerState.CYCLERUNNING if direction != Vector2.ZERO else PlayerState.CYCLEIDLE
+	elif not have_cycle :
+		current_player_state = PlayerState.WALKING if direction != Vector2.ZERO else PlayerState.IDLE
 	
 	# Update animation based on state and direction
 	var anim_direction = direction if direction != Vector2.ZERO else last_direction
